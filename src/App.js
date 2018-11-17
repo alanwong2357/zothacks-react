b1de98ac373c6078ed2dcc3d20045746b3727430
Alan Wong <alanwong2357@gmail.com>
2018-11-17 15:19:56 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
+// import logo from './logo.svg';
 import './App.css';
+import Reddit from './reddit'
 
-class App extends Component {
+// const CompononetExample = () => (<div>
+//   <h2>Your Feed</h2>
+// </div>)
+class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Reddit/>
+          {/*
+          // <CompononetExample/>
+          */}
         </header>
       </div>
     );
   }
+
+  componentDidMount() {
+    fetch('test-data.json')
+    .then(res => res.json())
+    .then(json => this.setState({ data: json }));
+
+    // .then((res) => res.json())
+    // .then((data) => {
+    //   console.log('data:', data);
+    // })
+  }
+
+  // componentDidMount() {
+  //   fetch('localhost://')
+  //     .then(response => response.json())
+  //     .then(data => this.setState({ data }));
+  // }
+
+
 }
 
 export default App;
+
+
+//fetch - request library
+//component will mount
+// get mount put data
+//lifecycle.mount
+//lifecycle documentation
---
+++ src/reddit.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Reddit extends React.Component {
+
+  // componentDidMount() {
+  //   fetch()
+  // }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2>Reddit</h2>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Reddit;
